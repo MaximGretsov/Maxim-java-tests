70825bb0d34880f9396be21e5f6fd14fa1aa7ce6
MaximGretsov <gretsovmaxim@gmail.com>
2026-07-30 00:41:53 +0300

    Update AdminPanel.java

--- src/main/java/ui/pages/AdminPanel.java
+++ src/main/java/ui/pages/AdminPanel.java
@@ -11,6 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class AdminPanel extends BasePage<AdminPanel>{
     private SelenideElement adminPanelText =  $(Selectors.byText("Admin Panel"));
     private SelenideElement addUserButton = $(Selectors.byText("Add User"));
+    private static final String USER_BADGE_TEXT_TEMPLATE = "%s\nUSER";
 
     @Override
     public String url() {
@@ -21,6 +22,16 @@ public class AdminPanel extends BasePage<AdminPanel>{
         usernameInput.sendKeys(username);
         passwordInput.sendKeys(password);
         addUserButton.click();
+        return this;
+    }
+
+    public AdminPanel shouldHaveUser(String username) {
+        getAllUsers()
+                .findBy(Condition.exactText(
+                        USER_BADGE_TEXT_TEMPLATE.formatted(username)
+                ))
+                .shouldBe(Condition.visible);
+
         return this;
     }
 
