0e7ea6fcbb8c02ea5bd7311cf1d6cd5976a3bdd7
MaximGretsov <gretsovmaxim@gmail.com>
2026-07-30 00:42:25 +0300

    Update BankAlert.java

--- src/main/java/ui/pages/BankAlert.java
+++ src/main/java/ui/pages/BankAlert.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum BankAlert {
     USER_CREATED_SUCCESSFULLY("✅ User created successfully!"),
-    USERNAME_MUST_BE_BETWEEN_3_AND_15_CHARACTERS("Username must be between 3 and 15 characters"),
+    USERNAME_MUST_BE_BETWEEN_3_AND_15_CHARACTERS("❌ Failed to create user:\n\n" +
+                    "• username: Username must be between 3 and 15 characters"),
     NEW_ACCOUNT_CREATED("✅ New Account Created! Account Number: "),
     GOOD_DEPOSIT("✅ Successfully deposited $%s to account ACC%s!"),
     BAD_DEPOSIT("❌ Please enter a valid amount."),
