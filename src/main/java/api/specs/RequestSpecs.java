76ec6c92e6feb83d4d2ef5c89daada3b07a3a0d4
Maxim Gretsov <gretsovmaxim@gmail.com>
2026-07-31 18:21:54 +0300

    Trigger test workflow

--- src/main/java/api/specs/RequestSpecs.java
+++ src/main/java/api/specs/RequestSpecs.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// Contains reusable request specifications for API tests.
 public class RequestSpecs {
     private static Map<String,String> authHeaders = new HashMap<>(Map.of("admin", "Basic YWRtaW46YWRtaW4="));
     private RequestSpecs(){};
