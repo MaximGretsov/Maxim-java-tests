63a488529328a0b0ef939ba43ff1a80763f1ad6c
MaximGretsov <gretsovmaxim@gmail.com>
2026-08-06 00:40:00 +0300

    Update RandomModelGenerator.java

--- src/main/java/api/generators/RandomModelGenerator.java
+++ src/main/java/api/generators/RandomModelGenerator.java
@@ -867,4 +867,18 @@ public final class RandomModelGenerator {
 
         return word.toString();
     }
+
+     // Метод для генерации некорректного имени
+    public static String generateInvalidName() {
+        int wordLength = RANDOM.nextInt(1, 2);
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder word = new StringBuilder();
+
+        for (int i = 0; i < wordLength; i++) {
+            int randomIndex = RANDOM.nextInt(letters.length());
+            word.append(letters.charAt(randomIndex));
+        }
+
+        return word.toString();
+    }
 }
