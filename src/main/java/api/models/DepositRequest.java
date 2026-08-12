e4d38458d9e624ca6de47a3d3b1251eacacf1c61
Maxim Gretsov <gretsovmaxim@gmail.com>
2026-08-12 07:11:58 +0300

    Add fraud check mock tests

--- src/main/java/api/models/DepositRequest.java
+++ src/main/java/api/models/DepositRequest.java
@@ -1,5 +1,6 @@
 package api.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class DepositRequest extends BaseModel{
+    @JsonProperty("accountId")
     private int id;
+
+    @JsonProperty("amount")
     private float balance;
+    private String description;
 }
---
+++ src/main/java/common/enums/FraudMockBehavior.java
@@ -0,0 +1,8 @@
+package common.enums;
+
+public enum FraudMockBehavior {
+    RESPONSE,
+    TIMEOUT,
+    CONNECTION_ERROR,
+    HTTP_ERROR
+}
